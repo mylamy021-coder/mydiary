2f6e2b2561dd50a1dbedcc0ac4f148041723926b
mylamy021-coder <mylamy021@gmail.com>
2026-03-08 15:52:55 +0600

    Delete my-diary-app directory

--- my-diary-app/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{textAlign:"center", marginTop:"50px"}}>
-      <h1>📖 Welcome to My Diary App</h1>
-      <p>Login to start writing your diary entries.</p>
-      <a href="/login">Go to Login</a>
-    </div>
-  );
-}
